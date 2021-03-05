fcc12fe56da43f56581ea05320f7de86ca8d5ad8
Cedric Fortuin <0322012@student.rocvantwente.nl>
2021-03-05 09:27:31 +0100

    update

---
+++ SPRINT 11/Sprint11_Exerc5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sprint11_Exerc5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Make the array
+            int[] array = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= array.Length - 2; j++) {
+                for (int i = 0; i <= array.Length - 2; i++) {
+                    if (array[i] > array[i + 1]) {
+                        temp= array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            
+            // Print the sorted array
+            Console.WriteLine("Sorted arrayay:");
+            foreach (int p in array)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
